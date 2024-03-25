9431f32e5f4015b7a9f4b2a5aedb95f8059b2b38
Paul Murray <paulthemurray@gmail.com>
2024-03-25 11:44:52 +1100

    adding to schema.prisma
    npm run build was not working, adding in previewFeatures = ['driverAdapters'] to get it to compile and deply

--- src/server/db.ts
+++ src/server/db.ts
@@ -6,12 +6,12 @@ import { env } from "~/env";
 
 const psClient = new Client({ url: env.DATABASE_URL });
 
-const createPrismaClient = () =>
-  new PrismaClient({
-    log:
-      env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-    adapter: new PrismaPlanetScale(psClient) as PrismaPlanetScale,
+const createPrismaClient = () => {
+  return new PrismaClient({
+    log: env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+    adapter: new PrismaPlanetScale(psClient),
   });
+};
 
 const globalForPrisma = globalThis as unknown as {
   prisma: ReturnType<typeof createPrismaClient> | undefined;
